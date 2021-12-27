5fc23eb42ab1bdad9b9c663e696ce823f1b7a163
김다은 <kimdaeun@indentcorp.com>
2021-12-27 16:49:11 +0900

    super recap

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  painGreetings(username);
+  painGreetings();
 }
 
-function painGreetings(username) {
+function painGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.classList.remove(HIDDEN_CLASSNAME);
   greeting.innerText = `Hello ${username}`;
 }
@@ -24,5 +24,5 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  painGreetings(savedUsername);
+  painGreetings();
 }
