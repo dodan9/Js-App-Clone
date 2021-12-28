5bd40731fd67b30936ff0fb052cc14adf5134ee7
김다은 <kimdaeun@indentcorp.com>
2021-12-28 15:25:09 +0900

    ✨ Add geolocation
    geolocation.getCurrentPosition으로 현재 경도와 위도 확인

---
+++ js/weather.js
@@ -0,0 +1,9 @@
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("You live in ", lat, lng);
+}
+function onGeoErr() {
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoErr);
